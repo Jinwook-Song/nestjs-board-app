e2056ad70fd1f654f0d33b12a4e2d30efbbd0b32
jinwook-song <wlsdnr129@naver.com>
2021-10-12 21:12:51 +0900

    Board DTO and CRUD

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v4 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
   private boards: Board[] = [];
 
+  // GET ALL Boards
   getAllBoards(): Board[] {
     return this.boards;
   }
 
-  createBoard(title: string, description: string): Board {
+  // Create a Board
+  createBoard({ title, description }: CreateBoardDto): Board {
     const board: Board = {
       id: uuid(),
       title,
@@ -18,6 +21,23 @@ export class BoardsService {
       status: BoardStatus.PUBLIC,
     };
     this.boards.push(board);
+    return board;
+  }
+
+  // GET a Board by ID
+  getBoardById(id: string): Board {
+    return this.boards.find((board) => board.id === id);
+  }
+
+  // Delete a Board by ID
+  deleteBoard(id: string) {
+    this.boards = this.boards.filter((board) => board.id !== id);
+  }
+
+  // Update a Board by ID
+  updaeteBoardStatus(id: string, status: BoardStatus): Board {
+    const board = this.getBoardById(id);
+    board.status = status;
     return board;
   }
 }
