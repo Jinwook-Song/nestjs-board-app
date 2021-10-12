386d2b62597c19657bea32cf0bd940b6a6811a8f
jinwook-song <wlsdnr129@naver.com>
2021-10-12 20:20:50 +0900

    Board Controller and Service

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000, () =>
+    console.log(`✅ Listening http://localhost:3000`),
+  );
+}
+bootstrap();
