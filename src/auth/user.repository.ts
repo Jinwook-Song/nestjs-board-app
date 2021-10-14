932bea4c1caa23e3f389ec4b4a6e2247ef6b4812
jinwook-song <wlsdnr129@naver.com>
2021-10-14 17:40:07 +0900

    bcrypt password

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,15 +1,18 @@
-import { EntityRepository, Repository } from 'typeorm';
-import { AuthCredentialsDto } from './dto/auth-credential.dto';
-import { User } from './user.entity';
 import {
   ConflictException,
   InternalServerErrorException,
 } from '@nestjs/common';
+import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import { User } from './user.entity';
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser({ username, password }: AuthCredentialsDto): Promise<void> {
-    const user = this.create({ username, password });
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = this.create({ username, password: hashedPassword });
 
     try {
       await this.save(user);
