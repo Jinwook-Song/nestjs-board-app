34ddc2a4930fe8f229dcfe2218ae9148b8473122
jinwook-song <wlsdnr129@naver.com>
2021-10-14 17:25:08 +0900

    signup unique user

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,11 +1,17 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
 
 @Entity()
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({ unique: true })
   username: string;
 
   @Column()
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,5 +1,24 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async createUser({ username, password }: AuthCredentialsDto): Promise<void> {
+    const user = this.create({ username, password });
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Existing username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+}
